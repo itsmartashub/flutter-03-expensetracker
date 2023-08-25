653c70bac8e6aad7e18ce17f876455fa56df7ac7
itsmartashub <coderawii@gmail.com>
2023-08-25 23:11:37 +0200

    05-002 starting setup & repetition time

---
+++ lib/expenses.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+//   _ExpensesState createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list... '),
+        ],
+      ),
+    );
+  }
+}
