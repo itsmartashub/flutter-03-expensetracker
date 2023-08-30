66ae1e8fbc48a9dfd1cff76f7df74e2bf481c387
itsmartashub <coderawii@gmail.com>
2023-08-30 18:57:33 +0200

    05-029 `CardTheme`, `TextTheme`, `elevatedButtonTheme`

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,41 @@ var kColorScheme =
 void main() {
   runApp(
     MaterialApp(
-      /* kada koristimo copyWith() na neki nacin overwritujemo originalnu material 3 temu, dakle koristimo original default flutter md temu, ali je overvritujemo sa svojim vrednostima */
+      /* kada koristimo copyWith() na neki nacin overwritujemo originalnu material 3 temu, dakle koristimo original default flutter md temu, ali je overvritujemo sa svojim vrednostima.
+      
+      Inace postoji neka inkonzistentnost kada se koristi .copyWith(), a kada .styleFrom() kao recimo sa buttons */
       theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        // svi skrins ce imati ovaj bg
-        // scaffoldBackgroundColor: Color.fromARGB(255, 193, 171, 241),
-      ),
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer),
+          ),
+          // textTheme: TextTheme()
+          textTheme: ThemeData().textTheme.copyWith(
+                // txt koji se korisi u AppBar-u
+                // titleLarge: ThemeData().textTheme.copyWith(),
+                //! inace ono gore foregroundColor za AppBarTheme OVERWRITUJE OVO OVDE SETOVANO ZA TEXT COLOR kad je AppBar u pitanju, ali ofc, za na drugim nekim mestima gde koristimo titleLarge nece overwritovati!!!
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 20,
+                ),
+              )
+          // svi skrins ce imati ovaj bg
+          // scaffoldBackgroundColor: Color.fromARGB(255, 193, 171, 241),
+          ),
       home: const Expenses(),
     ),
   );
