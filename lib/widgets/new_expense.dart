02d12fb6c885a88fdabc99f5bc5a6a7d2cb4caba
itsmartashub <coderawii@gmail.com>
2023-08-31 13:56:53 +0200

    06-006 `MediaQuery.of(context).viewInsets.` handling keyboard screen overlays

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -97,105 +97,120 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        // padding: const EdgeInsets.all(16),
-        // da kontent ne bi se overlapovao sa gore kamerom i sl
-        padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
-        child: Column(
-          //? TextField widget
-          /* Omogucava korisniku da unese text. Ima mnooogo propertyja */
-          children: [
-            TextField(
-              // onChanged: _saveTitleInput, // NACIN I
-              controller: _titleController, // NACIN II
-              maxLength: 50,
-              decoration: const InputDecoration(
-                label: Text('Title'),
-              ),
-            ),
-            Row(children: [
-              /* * takodje mora Expanded jer se ispostavilo da TextField widget ima problema kada je unutar row-a ovako, jer takodje zeli horizontalno da zauze sto je vise psrostora moguce, a Row difoltno ne ogranicava prostor koji sme da se zauzme, i to izaziva probleme kada flutter treba da renderuje ovu kombinaciju na ekranu. A ako ovde budemo koristili Expanded ovde on ce se pobrinuti da TextField zauzme koliko mesa mu je potrebno za kontent, a ne vise */
-              Expanded(
-                child: TextField(
+    /* ? MediaQuery.of(context).viewInsets
+    Kada sm ou landscape mode, keyboard zauzima vise od pola ekrana i ne mogu stvari da se unesu u input. Takodje, title je predugo, bespotrebno zauzima toliko mesta. Pa cemo staviti dinamicki padding onoliko koliko zauzima tastatura.
+    viewInsets omogucava informacije o UI elementima koji mozda overlapuju neke delove UI-a. Ako recimo koristimo .viewInsets.bottom, dobijemo bilo koji extra UI element koji overlapuje UI od dole, a to je ono sto recimo keyboard radi, slajduje od dole i prekriva UI.
+    ! Pa tako ovim dobijamo kolicinu prostora koju ta tastatura zauzima.
+    Pa cemo na ovih 16 padding od dole, dodati i onaj extra prostor koji tastatura zauzima. Medjutim, ne desava se nista.
+    ! Treba zapravo i ovaj widget tree koji je returned by a NewExpense widget, SCROLLABLE. Pa cemo Padding widget da wrapujemo u SingleChildScrollView widget sa Refactor...
+    
+    Medjutim, sada jos primecujemo da ModalSheet ne zauzima full height odnosno ne ide skroz do gore, ikao ajd, nije strasno, ali mi mozda zelimo da zauzima prostor skroz do gore. Pa cemo sad ovaj SingleChildScrollView da wrapujemo u novi widget SizedBox jer na njemu mozemo da stavimo height koji cemo staviti da bude double.infinity da bismo bili sigurnoi da ovo zauzima maximalno prostora koliko moze  */
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+
+    return SizedBox(
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+            // padding: const EdgeInsets.all(16),
+            // da kontent ne bi se overlapovao sa gore kamerom i sl
+            padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
+            child: Column(
+              //? TextField widget
+              /* Omogucava korisniku da unese text. Ima mnooogo propertyja */
+              children: [
+                TextField(
                   // onChanged: _saveTitleInput, // NACIN I
-                  controller: _amountController, // NACIN II
-                  keyboardType: TextInputType.number,
+                  controller: _titleController, // NACIN II
+                  maxLength: 50,
                   decoration: const InputDecoration(
-                    prefixText: '\$ ',
-                    label: Text('Amount'),
+                    label: Text('Title'),
                   ),
                 ),
-              ),
-              const SizedBox(width: 16),
-              //! posto je Row unutar Row-a, moramo koristiti Expanded!
-              Expanded(
-                child: Row(
-                  // da pushuje kontent na kraj (desno)
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  // centrira verticalno
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                Row(children: [
+                  /* * takodje mora Expanded jer se ispostavilo da TextField widget ima problema kada je unutar row-a ovako, jer takodje zeli horizontalno da zauze sto je vise psrostora moguce, a Row difoltno ne ogranicava prostor koji sme da se zauzme, i to izaziva probleme kada flutter treba da renderuje ovu kombinaciju na ekranu. A ako ovde budemo koristili Expanded ovde on ce se pobrinuti da TextField zauzme koliko mesa mu je potrebno za kontent, a ne vise */
+                  Expanded(
+                    child: TextField(
+                      // onChanged: _saveTitleInput, // NACIN I
+                      controller: _amountController, // NACIN II
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        prefixText: '\$ ',
+                        label: Text('Amount'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  //! posto je Row unutar Row-a, moramo koristiti Expanded!
+                  Expanded(
+                    child: Row(
+                      // da pushuje kontent na kraj (desno)
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      // centrira verticalno
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // sa ! govorimo dartu da vrednost nece biti null, a buni se jer smo stavili gore DateTime? _selectedDate; sto znaci ili da bude date ili null, ali smo ovim ternarnim operatorom rekli da nece biti null
+                        Text(_selectedDate == null
+                            ? 'No date selected'
+                            : formatter.format(_selectedDate!)),
+                        IconButton(
+                          onPressed: _presentDatePicker,
+                          icon: const Icon(Icons.calendar_month),
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+                const SizedBox(height: 16),
+                Row(
                   children: [
-                    // sa ! govorimo dartu da vrednost nece biti null, a buni se jer smo stavili gore DateTime? _selectedDate; sto znaci ili da bude date ili null, ali smo ovim ternarnim operatorom rekli da nece biti null
-                    Text(_selectedDate == null
-                        ? 'No date selected'
-                        : formatter.format(_selectedDate!)),
-                    IconButton(
-                      onPressed: _presentDatePicker,
-                      icon: const Icon(Icons.calendar_month),
-                    )
-                  ],
-                ),
-              )
-            ]),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                /* Category.values, .values je lista enum-a tj lista kategorija, a items zeli listu Dropdown menu itema, zato cemo koristiti .map() da transformisemo jedan tip podataka u drugi.
-                DropdownMenuItem je widget gde moramo da setujemo child parametar u drugi widget sto predstavlja ono sto ce biti prikazano na ekranu. category je enum a nama treba string posto koristimo u Text-u. pa cemo pretvoriti ovaj category u text sa flutter propertijme name na kog zovemo toString().
-                - Ovaj value property u DropdownMenuItem nije vidljiv korisniku, vec se cuva interno za svaki DMI, dakle ono sto je vidljivo je ovo child: Text. I to value predstvlja vrednost onog onChanged parametra koje se menja svaki x kad korisnik izabere neki od ovih DMI */
-                DropdownButton(
-                  // s ovim value se uveravamo da ce neki item da bude difoltno selektovan, a nece biti prazno (leisure je inicijalno)
-                  value: _selectedCategory,
-                  items: Category.values
-                      .map(
-                        (category) => DropdownMenuItem(
-                          value: category,
-                          child: Text(
-                            // category.name.toString(),
-                            category.name.toUpperCase(),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                  onChanged: (value) {
-                    if (value == null) {
-                      return;
-                    }
+                    /* Category.values, .values je lista enum-a tj lista kategorija, a items zeli listu Dropdown menu itema, zato cemo koristiti .map() da transformisemo jedan tip podataka u drugi.
+                    DropdownMenuItem je widget gde moramo da setujemo child parametar u drugi widget sto predstavlja ono sto ce biti prikazano na ekranu. category je enum a nama treba string posto koristimo u Text-u. pa cemo pretvoriti ovaj category u text sa flutter propertijme name na kog zovemo toString().
+                    - Ovaj value property u DropdownMenuItem nije vidljiv korisniku, vec se cuva interno za svaki DMI, dakle ono sto je vidljivo je ovo child: Text. I to value predstvlja vrednost onog onChanged parametra koje se menja svaki x kad korisnik izabere neki od ovih DMI */
+                    DropdownButton(
+                      // s ovim value se uveravamo da ce neki item da bude difoltno selektovan, a nece biti prazno (leisure je inicijalno)
+                      value: _selectedCategory,
+                      items: Category.values
+                          .map(
+                            (category) => DropdownMenuItem(
+                              value: category,
+                              child: Text(
+                                // category.name.toString(),
+                                category.name.toUpperCase(),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        if (value == null) {
+                          return;
+                        }
 
-                    setState(() {
-                      _selectedCategory = value;
-                    });
-                  },
-                ),
-                const Spacer(),
-                TextButton(
-                    onPressed: () {
-                      /* pop property zeli context onaj iz Widget build(BuildContext context). pop() uklanja ovaj overlays sa skrina */
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancel')),
-                ElevatedButton(
-                  // onPressed: () {
-                  //   // print(_enteredTitle); // NACIN I
-                  //   print(_titleController.text); // NACIN II
-                  //   print(_amountController.text);
-                  // },
-                  onPressed: _submitExpenseData,
-                  child: const Text('Save Expense'),
-                ),
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      },
+                    ),
+                    const Spacer(),
+                    TextButton(
+                        onPressed: () {
+                          /* pop property zeli context onaj iz Widget build(BuildContext context). pop() uklanja ovaj overlays sa skrina */
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancel')),
+                    ElevatedButton(
+                      // onPressed: () {
+                      //   // print(_enteredTitle); // NACIN I
+                      //   print(_titleController.text); // NACIN II
+                      //   print(_amountController.text);
+                      // },
+                      onPressed: _submitExpenseData,
+                      child: const Text('Save Expense'),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
